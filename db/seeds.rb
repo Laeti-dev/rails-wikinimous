343aa7bd1935b738ab575b594eed190da105ac3c
Laetitia Taddei <laetdi@gmail.com>
2022-01-11 11:48:17 +0900

    index route set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Adding 10 new articles"
+10.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraphs
+  )
+end
+puts "Done!"
